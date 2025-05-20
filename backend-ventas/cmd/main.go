b7105b07820dbdc93196ebaa0e69ee96a6dfd0b8
Kriptaman10 <benja.riquelme2001@gmail.com>
2025-05-19 20:27:07 -0400

    aaaa

---
+++ backend-ventas/cmd/main.go
@@ -0,0 +1,22 @@
+﻿package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "log"
+    "os"
+)
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{"message": "pong"})
+    })
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "3001"
+    }
+
+    log.Fatal(r.Run(":" + port))
+}
